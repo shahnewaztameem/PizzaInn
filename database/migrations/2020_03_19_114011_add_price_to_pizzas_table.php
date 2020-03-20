e8254403671c7bd2c881f57a70abc7d8837bbe51
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2020-03-20 23:07:37 +0600

    migration added

--- database/migrations/2020_03_19_114011_add_price_to_pizzas_table.php
+++ database/migrations/2020_03_19_114011_add_price_to_pizzas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePizzasTable extends Migration
+class AddPriceToPizzasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,8 @@ class CreatePizzasTable extends Migration
      */
     public function up()
     {
-        Schema::create('pizzas', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->string('type');
-            $table->string('base');
-            $table->string('name');
-        
+        Schema::table('pizzas', function (Blueprint $table) {
+            $table->string('price');
         });
     }
 
@@ -30,6 +25,8 @@ class CreatePizzasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('pizzas');
+        Schema::table('pizzas', function (Blueprint $table) {
+            //
+        });
     }
 }
